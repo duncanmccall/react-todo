8b56bc546b868edde0a0f75012fd1465bf7987b7
Duncan McCall <mccall.duncan@gmail.com>
2020-01-19 17:42:24 -0500

    created Todo and TodoItem components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './styles/reset.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Make dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Laundry',
+        completed: false
+      }     
+    ]
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    )
+  }
 }
 
 export default App;
